87a243ec4de0b5602d13474a52895afaca5080dd
Gabriel Ripka <ga_gripka@hotmail.com>
2023-03-15 08:06:30 -0300

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,48 +1,72 @@
-def calc():
+# Importar o módulo math para operações matemáticas avançadas
+import math
+
+# Obter a entrada do usuário para selecionar a operação 
+#                                           e verificar se é válida
+while True:
     try:
-        num1 = int(input("Digte o primeiro número: "))
-        op = input("Selecione a operação: (+, -, /, *): ")
-        num2 =  int(input("Digite o segundo número: "))
+        op = int(input(print("Selecione a operação: \n1. Somar \n2. Subtrair \n3. Multiplicar \n4. Dividir \n5. Exponenciação \n6. Raiz Quadrada \n7. Logaritmo")))
+        if op not in [1, 2, 3, 4, 5, 6, 7]: 
+            raise ValueError
+        break
     except ValueError:
-        print ("Caractere inválido")
-    pass
+        print ("Opção inválida, tente novamente!")
 
+# Definir as operações matemáticas
+while True:
+    try:
+        num1 = float(input("Digte o primeiro número: "))
+        if op != 6:
+            num2 =  float(input("Digite o segundo número: "))
+            if op == 4 and num2 == 0:
+                raise ZeroDivisionError
+        break
+    except ValueError:
+        print("Número inválido")
+    except ZeroDivisionError:
+        print ("Erro! Divisão por zero")
 
-    while True:
-        if op == "+":
-            resultado = num1 + num2
-        elif op == "-":
-            resultado = num1 - num2
-        elif op == "*":
-            resultado = num1 * num2
-        elif op == "/":
-            resultado = num1 / num2
-            if num2 == 0:
-                print("Erro: Divisão por zero!")
-                return
-            
-        else: 
-            print ("Deu não, tenta de novo")
-        print (f"O resultado é: {resultado}")
-        return menu()
+# Executar a operação selecionada
+if op == 1:
+    resultado = (num1 + num2)
+    sinalop = "+"
+elif op == 2:
+    resultado = (num1 - num2)
+    sinalop = "-"
+elif op == 3:
+    resultado = num1 * num2
+    sinalop = "*"
+elif op == 4:
+    resultado = num1 / num2
+    sinalop = "/"
+elif op == 5:
+    resultado = num1 ** num2
+    sinalop = "^"
+elif op == 6:
+    resultado = num1 ** 0.5
+    sinalop = "√"
+elif op == 7:
+    base = float(input("Digite a base do logaritmo: "))
+    resultado = math.log(num1, base)
+    sinalop = "log"
+    #return
+#return menu()
 
-def menu():
-    print("Selecione a operação.")
-    print("1.Somar")
-    print("2.Subtrair")
-    print("3.Multiplicar")
-    print("4.Dividir")
-    continuando = input("Selecione uma das opções acima: ")
-    if continuando == ("1"):
-        #inicio()
-        calc()
-    elif continuando == ("2"):
-        print("Até mais! :)")
-        exit()
-    else:
-        print("Entrada inválida. Por favor digite um número entre 1 e 5.")
-        return menu()
+# Exibir o resultado
+if op in [1, 2, 3, 4, 5, 7]:
+    print(f"{num1} {sinalop} {num2} = {resultado}")
+elif op == 6:
+    print(f"A raiz quadrada de {num1} é {resultado:.2f}")
 
-menu()
+#def menu():
+#    continuando = input("Iniciar a calculadora? \n1-Sim \n2-Não \n")
+#    if continuando == ("1"):
+#        calc()
+#    elif continuando == ("2"):
+#        print("Até mais! :)")
+#        exit()
+#    else:
+#        print("Entrada inválida. Por favor escolha 1 ou 2.")
+#        return menu()
 
-
+#menu():
